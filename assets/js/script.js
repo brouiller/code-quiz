7ab9a2bddfd54eca6b52aedae04e485fe7dc1995
Father Shark <11543424+brouiller@users.noreply.github.com>
2021-09-21 21:53:34 -0400

    fixed askQuestion and nextQuestion functions, removed eventListeners appropriately, decremented timer on incorrect answer

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,21 +7,16 @@ var answerThree = document.querySelector("#three");
 var answerFour = document.querySelector("#four");
 var scoreList = document.querySelector("#scores");
 var answerClass = document.querySelectorAll(".answer");
+var result = document.querySelector("#result");
 
 var time;
 var timer;
 var score;
 var gameWon = true;
 var qCount = 0;
+var check;
 
-var questionArray = ["question 1", "question 2", "question 3", "question 4", "question 5"];
-var answerArray = [
-  ["answer 1-1", "answer 1-2", "answer 1-3", "answer 1-4"],
-  ["answer 2-1", "answer 2-2", "answer 2-3", "answer 2-4"],
-  ["answer 3-1", "answer 3-2", "answer 3-3", "answer 3-4"],
-  ["answer 4-1", "answer 4-2", "answer 4-3", "answer 4-4"],
-  ["answer 5-1", "answer 5-2", "answer 5-3", "answer 5-4"],
-];
+
 var questionAnswers = [
   {
     question: "1. Which type of variable is 'var x = 1;'?",
@@ -49,8 +44,8 @@ var questionAnswers = [
   },
   {
     question: "4. How many times will this loop run? for(var i = 0; i <= 4, i++)",
-    answer1: "1",
-    answer2: "0",
+    answer1: "2",
+    answer2: "3",
     answer3: "5",
     answer4: "4",
     correct: "5"
@@ -65,64 +60,91 @@ var questionAnswers = [
   }
 ];
 
-
 function startQuiz() {
-  time = 10;
+  time = 100;
   startCountdown();
   var cut = document.getElementById("start-button");
   cut.remove();
   askQuestion(qCount);
-}
-
+  console.log("startQ: " + qCount);
+};
 function startCountdown() {
   timer = setInterval(function () {
     time--;
     countDown.textContent = time;
     if (time >= 0) {
-      if (gameWon && time < 0) {
+      if (time < 0) {
         score = time;
         clearInterval(timer);
-        saveScore();
-      }
-    }
+      };
+    };
     if (time === 0) {
       clearInterval(timer);
       gameLost();
     }
   }, 1000);
-}
+};
 
 function saveScore() {
   console.log("score saved");
-}
+};
 
-function gameLost() {}
+function gameLost() {
+  console.log("game lost");
+};
 
 function askQuestion(x) {
-  if (x < questionAnswers.length) {
+  console.log("x: " + x);
+  console.log("askQ count: " + qCount);
+  answerOne.removeEventListener("click", nextQuestion);
+  answerTwo.removeEventListener("click", nextQuestion);
+  answerThree.removeEventListener("click", nextQuestion);
+  answerFour.removeEventListener("click", nextQuestion);
+  if (x < 5) {
     question.textContent = questionAnswers[x].question;
+    //loop to style border of answer boxes
     for (var i = 0; answerClass.length > i; i++) {
       answerClass[i].style.border = "1px solid var(--honeyDew)";
-    }
+    };
     answerOne.textContent = questionAnswers[x].answer1;
     answerOne.addEventListener("click", nextQuestion);
+    if (questionAnswers[x].answer1 === questionAnswers[x].correct) {
+      check = "one";
+    };
     answerTwo.textContent = questionAnswers[x].answer2;
     answerTwo.addEventListener("click", nextQuestion);
+    if (questionAnswers[x].answer2 === questionAnswers[x].correct) {
+      check = "two";
+    };
     answerThree.textContent = questionAnswers[x].answer3;
     answerThree.addEventListener("click", nextQuestion);
+    if (questionAnswers[x].answer3 === questionAnswers[x].correct) {
+      check = "three";
+    };
     answerFour.textContent = questionAnswers[x].answer4;
     answerFour.addEventListener("click", nextQuestion);
-    qCount++;
-  }
-}
+    if (questionAnswers[x].answer4 === questionAnswers[x].correct) {
+      check = "four";
+    };
+  };
+};
 
-function nextQuestion() {
-  if (qCount < questionAnswers.length) {
-    var z = qCount;
-    askQuestion(z);
-  } else {
-    saveScore();
-  }
-}
+function nextQuestion(event) {
+  console.log(event.target.id);
+  qCount++;
+    if (event.target.id === check) {
+      result.textContent = "correct";
+      console.log("true: " + time);
+    } else {
+      result.textContent = "incorrect";
+      console.log("false: " + time);
+      time = time - 10;
+  };
+    if (qCount < 6) {
+      askQuestion(qCount);
+    } else {
+      saveScore();
+  };
+};
 
 startButton.addEventListener("click", startQuiz);
